10ceccd0fa8f9f219327a0daf47bc6f98ac79b2b
Hiba <160429893+hibaw4@users.noreply.github.com>
2026-08-24 20:55:15 +0100

    Add: is-palindrome - submission-27

---
+++ Data Structures & Algorithms/is-palindrome/submission-27.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join(c.lower() for c in s if c.isalnum())
+        n = len(s)
+        l, r = 0, n-1
+
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            else:
+                l += 1
+                r -= 1
+        return True
